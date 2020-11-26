5f408d42ec2aa82fcb1b5d31d76e520c0451e4b5
unknown <yosuapurnomo321@gmail.com>
2020-11-26 22:39:12 +0700

    model

--- src/com/warehousemanajemen/main.java
+++ src/com/warehousemanajemen/main.java
@@ -1,13 +1,15 @@
 package com.warehousemanajemen;
 
 import com.warehousemanajemen.view.Insert;
+import database.koneksi;
 public class main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Insert p = new Insert();
+//        Insert p = new Insert();
+            koneksi con = new koneksi();
     }
     
 }
---
+++ src/model/transaksi.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class transaksi {
+    private int id;
+    private Date tgl_masuk;
+    private Date tgl_keluar;
+    private int jumlah;
+    private int produk;
+    private int user;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getTgl_masuk() {
+        return tgl_masuk;
+    }
+
+    public void setTgl_masuk(Date tgl_masuk) {
+        this.tgl_masuk = tgl_masuk;
+    }
+
+    public Date getTgl_keluar() {
+        return tgl_keluar;
+    }
+
+    public void setTgl_keluar(Date tgl_keluar) {
+        this.tgl_keluar = tgl_keluar;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public int getProduk() {
+        return produk;
+    }
+
+    public void setProduk(int produk) {
+        this.produk = produk;
+    }
+
+    public int getUser() {
+        return user;
+    }
+
+    public void setUser(int user) {
+        this.user = user;
+    }
+    
+    
+}
